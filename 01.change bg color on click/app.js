b5434dc285171cc56160e7e50c42dcf4387fd51c
refat alsakka <refatalsakka@gmail.com>
2020-04-14 18:04:24 +0200

    change to Regex

--- 01.change bg color on click/app.js
+++ 01.change bg color on click/app.js
@@ -1,11 +1,6 @@
 document.querySelector('#btn').addEventListener('click', changeColor);
 
 function changeColor(){
-    let r = Math.round( Math.random() * 256 );
-    let g = Math.round( Math.random() * 256 );
-    let b = Math.round( Math.random() * 256 );
-    let color = `rgb(${r},${g},${b})`;
-    // let color = 'rgb'+'('+r+','+g+','+b+')';
-    // console.log(color);
+    let color = `rgb(x,x,x)`.replace(/x/gi, _ => Math.round(Math.random() * 256));
     document.body.style.backgroundColor = color;
 }
