7b78812b0480b263aeda7285b076c56ef17d9b07
Refat Alsakka <ralsakka@cuxonline.de>
2020-04-27 17:23:02 +0200

    js

--- 05.Portfolio Gallery filtering/js/app.js
+++ 05.Portfolio Gallery filtering/js/app.js
@@ -1,20 +1,60 @@
-const filterBtn  = document.querySelector("#filter-btns").children;
-const item = document.querySelector(".gallery").children;
-for (let i = 0 ; i < filterBtn.length ; i++){
-    filterBtn[i].addEventListener('click' , function() {
-        for ( let j = 0; j < filterBtn.length; j++){
-            filterBtn[j].classList.remove('active');
-        }
-        this.classList.add('active');
-        const target = this.getAttribute('data-target');
-        for ( let n = 0; n < item.length ; n++ ){
-            item[n].style.display = "none";
-            if( item[n].getAttribute('data-id') == target ){
-                item[n].style.display = "block";
-            }
-            if (target == 'all'){
-                item[n].style.display = "block";
-            }
-        }
+const list = function() {
+
+    const items = [...document.querySelectorAll(".item")];
+
+    function loop(array, callback) {
+        array.forEach(item => {
+            callback(item);
+        });
+    }
+
+    function diable(item) {
+        item.classList.add('disable');
+        item.addEventListener('animationend', _ => item.style.display = 'none');
+    }
+
+    function active(item) {
+        item.classList.remove('disable');
+        item.style.display = 'block';
+    }
+
+    function all() {
+        loop(items, item => {
+            active(item);
+        });
+    }
+ 
+    function web() {
+        all();
+        loop(items.filter(item => item.dataset.id !== 'web'), item => {
+            diable(item);
+        });
+    }
+
+    function responsive(){
+        all();
+        loop(items.filter(item => item.dataset.id !== 'responsive'), item => {
+            diable(item);
+        });
+    }
+
+    function uiux(){
+        all();
+        loop(items.filter(item => item.dataset.id !== 'uiux'), item => {
+            diable(item);
+        });
+    }
+  
+    return {
+        all,
+        responsive,
+        web,
+        uiux,
+    }
+}();
+
+const filterBtn  = [...document.querySelectorAll("#filter-btns li")].forEach(li => {
+    li.addEventListener('click', () => {
+        eval(`list.${li.dataset.target}()`);
     });
-}
+});;
